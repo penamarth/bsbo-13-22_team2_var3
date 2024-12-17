664efdeb8e9781852f58a157dfe7f557cce802b4
InspectorBulka <artemlomakin2019@yandex.ru>
2024-12-18 02:18:56 +0300

    новый код

--- code.cs
+++ code.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +16,12 @@ public interface ISubject
 
 public class Поликлиника : ISubject
 {
-    public List<Пациент> Пациенты { get; set; }
+    public List<Пациент> Пациенты { get; private set; }
     private Dictionary<DateTime, List<TimeSpan>> доступныеВремена;
     private List<IObserver> observers;
+
+    public Врач Врач { get; private set; }
+    public Администратор Администратор { get; private set; }
 
     public Поликлиника()
     {
@@ -28,8 +30,23 @@ public class Поликлиника : ISubject
         observers = new List<IObserver>();
     }
 
-    public void ПостановкаНаУчет(Администратор администратор, string имя, string фамилия, string отчество, DateTime датаРождения, int id)
+    public void НазначитьВрача(string фио)
     {
+        Врач = new Врач { ФИО = фио };
+        Attach(Врач);
+        Console.WriteLine($"Назначен врач: {Врач.ФИО}");
+    }
+
+    public void НазначитьАдминистратора(string фио)
+    {
+        Администратор = new Администратор { ФИО = фио };
+        Attach(Администратор);
+        Console.WriteLine($"Назначен администратор: {Администратор.ФИО}");
+    }
+
+    public void ПостановкаНаУчет(string имя, string фамилия, string отчество, DateTime датаРождения)
+    {
+        int id = Пациенты.Count + 1;
         var новыйПациент = new Пациент
         {
             ID = id,
@@ -40,7 +57,7 @@ public class Поликлиника : ISubject
         };
         Пациенты.Add(новыйПациент);
         Console.WriteLine($"{новыйПациент.Фамилия} {новыйПациент.Имя} {новыйПациент.Отчество} добавлен(а) на учет.");
-        администратор.УведомитьОДобавлении();
+        NotifyObservers("Добавлен новый пациент.");
     }
 
     public void ПоказатьПациентов()
@@ -52,7 +69,7 @@ public class Поликлиника : ISubject
         }
     }
 
-    public void СнятьСУчета(Администратор администратор)
+    public void СнятьСУчета()
     {
         ПоказатьПациентов();
 
@@ -65,12 +82,11 @@ public class Поликлиника : ISubject
             {
                 Пациенты.Remove(пациент);
                 Console.WriteLine($"Пациент {пациент.Фамилия} {пациент.Имя} {пациент.Отчество} снят с учета.");
-                администратор.УведомитьОСнятии();
+                NotifyObservers("Пациент снят с учета.");
             }
             else
             {
                 Console.WriteLine("Пациент не найден.");
-                администратор.УведомитьОНеверномСнятии();
             }
         }
         else
@@ -79,22 +95,32 @@ public class Поликлиника : ISubject
         }
     }
 
-    public bool ЗаписьНаПрием(Администратор администратор, Пациент пациент, Врач врач, DateTime дата, TimeSpan время)
+    public void ЗаписьНаПрием(int patientId, DateTime дата, TimeSpan время)
     {
+        Пациент пациент = Пациенты.FirstOrDefault(p => p.ID == patientId);
+        if (пациент == null)
+        {
+            Console.WriteLine("Пациент не найден.");
+            return;
+        }
+
+        if (Врач == null)
+        {
+            Console.WriteLine("Врач не назначен.");
+            return;
+        }
+
         if (ПроверитьДоступность(дата, время))
         {
-            var запись = new ЗаписьНаПрием(пациент, врач, дата, время);
+            var запись = new ЗаписьНаПрием(пациент, Врач, дата, время);
             запись.Attach(пациент);
-            запись.Attach(врач);
+            запись.Attach(Врач);
             запись.ПодтвердитьЗапись();
-
             ОбновитьДоступностьПослеЗаписи(дата, время);
-            return true;
         }
         else
         {
-            администратор.УведомитьОНедоступности();
-            return false;
+            Console.WriteLine("Выбранное время недоступно.");
         }
     }
 
@@ -113,23 +139,6 @@ public class Поликлиника : ISubject
         {
             доступныеВремена[дата].Remove(время);
         }
-    }
-
-    public void ОбработатьЗапросНаУчетФактаПриема(Врач врач, Пациент пациент, МедКарта медКарта, string запись)
-    {
-        if (ПроверитьЗаписьПациента(пациент))
-        {
-            врач.ДобавитьЗаписьВКарту(медКарта, $"{DateTime.Now}: {запись}");
-            Console.WriteLine("Учет факта приема завершен.");
-        }
-        else
-        {
-            Console.WriteLine("Пациент не записан.");
-        }
-    }
-    private bool ПроверитьЗаписьПациента(Пациент пациент)
-    {
-        return Пациенты.Contains(пациент);
     }
 
     public void Attach(IObserver observer)
@@ -167,7 +176,6 @@ public class ЗаписьНаПрием : ISubject
         this.время = время;
     }
 
-
     public void ПодтвердитьЗапись()
     {
         string message = $"Запись на прием: Пациент {пациент.Имя} {пациент.Фамилия} к врачу {врач.ФИО} на {дата.ToShortDateString()} в {время}.";
@@ -193,7 +201,6 @@ public class ЗаписьНаПрием : ISubject
     }
 }
 
-
 public class Пациент : IObserver
 {
     public int ID { get; set; }
@@ -201,11 +208,6 @@ public class Пациент : IObserver
     public string Фамилия { get; set; }
     public string Отчество { get; set; }
     public DateTime ДатаРождения { get; set; }
-
-    public void ЗапросНаПостановкуНаУчет()
-    {
-        Console.WriteLine($"{Фамилия} {Имя} {Отчество} желает встать на учет.");
-    }
 
     public void Update(string message)
     {
@@ -217,69 +219,19 @@ public class Врач : IObserver
 {
     public string ФИО { get; set; }
 
-    public void ДобавитьЗаписьВКарту(МедКарта медКарта, string запись)
-    {
-        медКарта.ДобавитьЗапись(запись);
-    }
-
     public void Update(string message)
     {
         Console.WriteLine($"[Уведомление для врача] {ФИО}: {message}");
     }
 }
 
-public class Администратор
+public class Администратор : IObserver
 {
     public string ФИО { get; set; }
 
-    public void ПодтвердитьЗапись(Пациент пациент, DateTime дата, TimeSpan время)
+    public void Update(string message)
     {
-        Console.WriteLine($"Запись подтверждена для пациента {пациент.Имя} {пациент.Фамилия} на {дата.ToShortDateString()} в {время}.");
-    }
-
-    public void УведомитьОНедоступности()
-    {
-        Console.WriteLine("Выбранное время недоступно.");
-    }
-
-    public void УведомитьОДобавлении()
-    {
-        Console.WriteLine("Пациент успешно добавлен.");
-    }
-
-    public void УведомитьОСнятии()
-    {
-        Console.WriteLine("Пациент успешно снят с учета.");
-    }
-
-    public void УведомитьОНеверномСнятии()
-    {
-        Console.WriteLine("Не удалось снять с учета. Пациент не найден.");
-    }
-}
-
-public class МедКарта
-{
-    public List<string> Записи { get; set; }
-
-    public МедКарта()
-    {
-        Записи = new List<string>();
-    }
-
-    public void ДобавитьЗапись(string запись)
-    {
-        Записи.Add(запись);
-        Console.WriteLine($"Добавлена запись: {запись}");
-    }
-
-    public void ПоказатьЗаписи()
-    {
-        Console.WriteLine("Записи в медицинской карте:");
-        foreach (var запись in Записи)
-        {
-            Console.WriteLine(запись);
-        }
+        Console.WriteLine($"[Уведомление для администратора] {ФИО}: {message}");
     }
 }
 
@@ -287,124 +239,54 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int id = 0;
         Поликлиника поликлиника = new Поликлиника();
-        Врач врач = new Врач { ФИО = "Иванов Иван" };
-        Пациент пациент = new Пациент { Имя = "Иван", Фамилия = "Иванов", Отчество = "Иванович" };
-        Администратор администратор = new Администратор { ФИО = "Сидоров Алексей" };
 
-        поликлиника.Пациенты.Add(пациент);
+        поликлиника.НазначитьВрача("Иванов Иван Иванович");
+        поликлиника.НазначитьАдминистратора("Сидоров Алексей Петрович");
 
         while (true)
         {
-            Console.WriteLine("Выберите пользователя:");
-            Console.WriteLine("1. Администратор");
-            Console.WriteLine("2. Пациент");
-            Console.WriteLine("3. Врач");
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Добавить пациента на учет");
+            Console.WriteLine("2. Показать пациентов");
+            Console.WriteLine("3. Снять пациента с учета");
+            Console.WriteLine("4. Записать пациента на прием");
             Console.WriteLine("0. Выход");
 
             string выбор = Console.ReadLine();
             switch (выбор)
             {
                 case "1":
-                    Console.WriteLine("Выбрано: Администратор.");
-                    Console.WriteLine("1. Поставить на учет");
-                    Console.WriteLine("2. Снять с учета");
-                    Console.WriteLine("3. Запись пациента на приём");
-                    Console.WriteLine("4. Показать список пациентов");
-                    string выборАдм = Console.ReadLine();
-                    if (выборАдм == "1")
-                    {
-                        Console.WriteLine("Введите имя:");
-                        string имя = Console.ReadLine();
-                        Console.WriteLine("Введите фамилию:");
-                        string фамилия = Console.ReadLine();
-                        Console.WriteLine("Введите отчество:");
-                        string отчество = Console.ReadLine();
-                        Console.WriteLine("Введите дату рождения (гггг,мм,дд):");
-                        DateTime датаРождения = DateTime.Parse(Console.ReadLine());
-                        id = id + 1;
-                        поликлиника.ПостановкаНаУчет(администратор, имя, фамилия, отчество, датаРождения, id);
-                    }
-                    else if (выборАдм == "2")
-                    {
-                        поликлиника.СнятьСУчета(администратор);
-                    }
-                    else if (выборАдм == "3")
-                    {
-                        Console.WriteLine("Введите дату приёма (гггг,мм,дд):");
-                        DateTime желаемаяДата = DateTime.Parse(Console.ReadLine());
-
-                        Console.WriteLine("Введите время приёма (чч:мм):");
-                        TimeSpan желаемоеВремя = TimeSpan.Parse(Console.ReadLine());
-
-                        Console.WriteLine("Введите ID пациента:");
-                        int patientId = int.TryParse(Console.ReadLine(), out patientId) ? patientId : -1;
-                        var выбранныйПациент = поликлиника.Пациенты.FirstOrDefault(p => p.ID == patientId);
-
-                        if (выбранныйПациент != null)
-                        {
-                            if (!поликлиника.ЗаписьНаПрием(администратор, выбранныйПациент, врач, желаемаяДата, желаемоеВремя))
-                            {
-                                Console.WriteLine("Попробуйте другое время.");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Пациент с таким ID не найден.");
-                        }
-                    }
-                    else if (выборАдм == "4")
-                    {
-                        поликлиника.ПоказатьПациентов();
-                    }
+                    Console.WriteLine("Введите имя:");
+                    string имя = Console.ReadLine();
+                    Console.WriteLine("Введите фамилию:");
+                    string фамилия = Console.ReadLine();
+                    Console.WriteLine("Введите отчество:");
+                    string отчество = Console.ReadLine();
+                    Console.WriteLine("Введите дату рождения (гггг,мм,дд):");
+                    DateTime датаРождения = DateTime.Parse(Console.ReadLine());
+                    поликлиника.ПостановкаНаУчет(имя, фамилия, отчество, датаРождения);
                     break;
 
                 case "2":
-                    Console.WriteLine("Выбрано: Пациент.");
-                    Console.WriteLine("1. Запрос на постановку на учет");
-                    string выборПац = Console.ReadLine();
-                    if (выборПац == "1")
-                    {
-                        Console.WriteLine("Введите имя:");
-                        string имя = Console.ReadLine();
-                        Console.WriteLine("Введите фамилию:");
-                        string фамилия = Console.ReadLine();
-                        Console.WriteLine("Введите отчество:");
-                        string отчество = Console.ReadLine();
-                        Console.WriteLine("Введите дату рождения (гггг,мм,дд):");
-                        DateTime датаРождения = DateTime.Parse(Console.ReadLine());
-
-                        Пациент новыйПациент = new Пациент { Имя = имя, Фамилия = фамилия, Отчество = отчество, ДатаРождения = датаРождения };
-                        новыйПациент.ЗапросНаПостановкуНаУчет();
-                    }
+                    поликлиника.ПоказатьПациентов();
                     break;
-                case "3":
-                    Console.WriteLine("Выбрано: Врач.");
-                    Console.WriteLine("1. Учет факта приема пациента");
-                    string выборВрач = Console.ReadLine();
-                    if (выборВрач == "1")
-                    {
-                        Console.WriteLine("Введите ID пациента:");
-                        int patientId = int.TryParse(Console.ReadLine(), out patientId) ? patientId : -1;
-                        var выбралПациент = поликлиника.Пациенты.FirstOrDefault(p => p.ID == patientId);
 
-                        if (выбралПациент != null)
-                        {
-                            Console.WriteLine("Введите данные осмотра:");
-                            string запись = Console.ReadLine();
-                            МедКарта медКарта = new МедКарта();
-                            поликлиника.ОбработатьЗапросНаУчетФактаПриема(врач, выбралПациент, медКарта, запись);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Пациент с таким ID не найден.");
-                        }
-                    }
+                case "3":
+                    поликлиника.СнятьСУчета();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Введите ID пациента:");
+                    int patientId = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите дату приема (гггг,мм,дд):");
+                    DateTime дата = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите время приема (чч:мм):");
+                    TimeSpan время = TimeSpan.Parse(Console.ReadLine());
+                    поликлиника.ЗаписьНаПрием(patientId, дата, время);
                     break;
 
                 case "0":
-                    Console.WriteLine("Выход.");
                     return;
 
                 default:
@@ -412,7 +294,7 @@ public class Program
                     break;
             }
 
-            Console.WriteLine("Нажмите любую клавишу, чтобы вернуться в меню...");
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
             Console.ReadKey();
             Console.Clear();
         }
